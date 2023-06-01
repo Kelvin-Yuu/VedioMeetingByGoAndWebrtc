0b81dfa6a9326cc6a7fdff5efae4e6df421ecc1e
Kelvin-Yuu <kelvinu123456@gmail.com>
2023-06-01 20:40:06 +0800

    2023-06-01 Init

---
+++ internal/models/RoomBasic.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type RoomBasic struct {
+	gorm.Model
+	Identity string    `gorm:"column:identity;type:varchar(36);uniqueIndex;not null" json:"identity"`
+	Name     string    `gorm:"column:name;type:varchar(100);not null" json:"name"`
+	BeginAt  time.Time `gorm:"column:begin_at;type:datetime" json:"begin_at"`
+	EndAt    time.Time `gorm:"column:end_at;type:datetime" json:"end_at"`
+	CreateId uint      `gorm:"column:create_id;type:int(11);not null" json:"create_id"` // 创建者的唯一标识
+}
+
+func (table *RoomBasic) TableName() string {
+	return "room_basic"
+}
